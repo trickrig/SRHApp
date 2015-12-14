ecd7f614cf4b4e8df15236fbebb8ea3c91f4837f
Hendrik <hendrik.schmidl@mailbox.tu-dresden.de>
2015-12-14 14:06:53 +0100

    now everything is in SettingsTab.java and tabs work again. no access to the profile manager, changes will be saved to a local setting and profile

--- app/src/main/java/de/srh/srha/MainActivity.java
+++ app/src/main/java/de/srh/srha/MainActivity.java
@@ -28,9 +28,16 @@ import de.srh.srha.tabs.StartTab;
 
 public class MainActivity extends FragmentActivity {
 
+    /*
+    Tabs müssen gemacht werden
+    alles ordentlich und profilname hoch
+
+    alle aktuellen profile müssten geladen werden mit knopf neues profil hinzufügen mit eigenschaften des currentProfils
+     */
+
     private FragmentTabHost mTabHost;
 
-    private Settings settings = new Settings(false,false,false, false, false, 0);
+    private Settings settings;
     private Profile profile = new Profile(null, null, null, null, null,null);
 
     private Switch wifiSwitch,bluetoothSwitch, gpsSwitch, mobileSwitch, vibrationSwitch;
@@ -50,7 +57,6 @@ public class MainActivity extends FragmentActivity {
         //TODO:can i split this into SettingsTab.java and ServiceTab.java?
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        manager = new ProfileManager(getApplicationContext());
 
         mTabHost = (FragmentTabHost) findViewById(android.R.id.tabhost);
         mTabHost.setup(this, getSupportFragmentManager(), android.R.id.tabcontent);
@@ -64,106 +70,5 @@ public class MainActivity extends FragmentActivity {
         mTabHost.addTab(
                 mTabHost.newTabSpec("setting").setIndicator("Settings", null),
                 SettingsTab.class, null);
-
-        //set view on settingstab_layout. else he will search switches on activity_main!
-        setContentView(R.layout.settingstab_layout);
-
-        //detect all switches
-        wifiSwitch = (Switch) findViewById(R.id.wifiSwitch);
-        bluetoothSwitch = (Switch) findViewById(R.id.bluetoothSwitch);
-        gpsSwitch = (Switch) findViewById(R.id.gpsSwitch);
-        mobileSwitch = (Switch) findViewById(R.id.mobileSwitch);
-        vibrationSwitch = (Switch) findViewById(R.id.mobileSwitch);
-
-        //give every switch a changelistener and send information to settings
-        wifiSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                settings.setWifiOn(isChecked);
-            }
-        });
-
-        bluetoothSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                settings.setBluetoothOn(isChecked);
-            }
-        });
-
-        gpsSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                settings.setGpsOn(isChecked);
-            }
-        });
-
-        mobileSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                settings.setMobileDataOn(isChecked);
-            }
-        });
-
-        vibrationSwitch.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                settings.setVibrateOn(isChecked);
-                //TODO: if checked, volume seekbar invisible
-            }
-        });
-
-        //detect seekbar
-        volumeSeekBar = (SeekBar) findViewById(R.id.volumeSeekBar);
-
-        //give seekbar a changelistener and send information to settings & on ui
-        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                volumeTextView = (TextView) findViewById(R.id.volumeTextView);
-                volumeTextView.setText("Volume: "+progress);
-                //possible int: 0 - 100;
-                settings.setRingVolume(progress);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {}
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {}
-        });
-
-        //detect edit Text
-        preferredArrival = (EditText) findViewById(R.id.zielhaltEditText);
-        preferredDeparture = (EditText) findViewById(R.id.starthaltEditText);
-        profilName = (EditText) findViewById(R.id.profilNameEditText);
-
-        //detect spinner, actually not safe
-        spinner = (Spinner) findViewById(R.id.spinner);
-        list = new ArrayList<String>();
-        adapter = new ArrayAdapter<String>(this, R.layout.settingstab_layout, list);
-        spinner.setAdapter(adapter);
-    }
-
-
-    public void createProfile(View v){
-
-        LinkedList list;
-
-        if(v.getId()==R.id.createProfilButton){
-            //TODO: switches haben funktion setTextOn setTextOff, automatisieren
-            //TODO: text der edit felder automatisch löschen
-            //where do i get id from, null has to be getId() Function
-            dvb test = new dvb();
-            String IdArrival = test.getIdFromName(preferredArrival.getText().toString());
-            String IdDep = test.getIdFromName(preferredDeparture.getText().toString());
-            profile.setPreferredArrival(IdArrival, preferredArrival.getText().toString());
-            profile.setPreferredDeparture(IdDep, preferredDeparture.getText().toString());
-//            preferredArrival.setText(IdArrival);
-//            preferredDeparture.setText(IdDep);
-            profile.setProfileName(profilName.getText().toString());
-            manager.updateProfile(profile, settings);
-//            RoutePlan rout = test.getRoute(IdArrival, IdDep);
-//            preferredDeparture.setText(rout.getDestinationTime());
-        }
     }
 }
--- app/src/main/java/de/srh/srha/tabs/SettingsTab.java
+++ app/src/main/java/de/srh/srha/tabs/SettingsTab.java
@@ -2,14 +2,45 @@ package de.srh.srha.tabs;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTabHost;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.util.LinkedList;
+import java.util.List;
+
 import de.srh.srha.R;
+import de.srh.srha.model.Profile;
+import de.srh.srha.model.ProfileManager;
+import de.srh.srha.model.Settings;
+import de.srh.srha.model.dvb;
+
 
 public class SettingsTab extends Fragment {
+
+    private FragmentTabHost mTabHost;
+
+    private Settings settings = new Settings(false, false, false, false, false, 0);
+    private Profile profile = new Profile(null, null, null, null, null, null);
+
+    private Switch wifiSwitch,bluetoothSwitch, gpsSwitch, mobileSwitch, vibrationSwitch;
+    private SeekBar volumeSeekBar;
+    private TextView volumeTextView;
+    private EditText preferredDeparture, preferredArrival, profilName;
+    private Spinner spinner;
+
+    private ArrayAdapter<String> adapter;
+    private List<String> list;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -19,8 +50,119 @@ public class SettingsTab extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View v = inflater.inflate(R.layout.settingstab_layout, container, false);
+        final View v = inflater.inflate(R.layout.settingstab_layout, container, false);
+
+        //TODO manager = new ProfileManager(getApplication());
+        //TODO profile = manager.getCurrentProfile();
+        //TODO settings = profile.settingsManager.getSettings();
+
+        //detect all switches
+        wifiSwitch = (Switch) v.findViewById(R.id.wifiSwitch);
+        bluetoothSwitch = (Switch) v.findViewById(R.id.bluetoothSwitch);
+        gpsSwitch = (Switch) v.findViewById(R.id.gpsSwitch);
+        mobileSwitch = (Switch) v.findViewById(R.id.mobileSwitch);
+        vibrationSwitch = (Switch) v.findViewById(R.id.mobileSwitch);
+
+        //detect edit Text
+        preferredArrival = (EditText) v.findViewById(R.id.zielhaltEditText);
+        preferredDeparture = (EditText) v.findViewById(R.id.starthaltEditText);
+        profilName = (EditText) v.findViewById(R.id.profilNameEditText);
+
+        //detect seekbar & text
+        volumeSeekBar = (SeekBar) v.findViewById(R.id.volumeSeekBar);
+        volumeTextView = (TextView) v.findViewById(R.id.volumeTextView);
+
+        setUi();
+
+        //give every switch a changelistener and send information to settings
+        wifiSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                settings.setWifiOn(isChecked);
+            }
+        });
+
+        bluetoothSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                settings.setBluetoothOn(isChecked);
+            }
+        });
+
+        gpsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                settings.setGpsOn(isChecked);
+            }
+        });
+
+        mobileSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                settings.setMobileDataOn(isChecked);
+            }
+        });
+
+        vibrationSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                settings.setVibrateOn(isChecked);
+                //TODO: if checked, volume seekbar invisible
+            }
+        });
+
+        //give seekbar a changelistener and send information to settings & on ui
+        volumeSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                volumeTextView = (TextView) v.findViewById(R.id.volumeTextView);
+                volumeTextView.setText("Volume: " + progress);
+                //possible int: 0 - 100;
+                //TODO settings.setRingVolume(progress);
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+
         return v;
+    }
+
+    public void createProfile(View v){
+
+        LinkedList list;
+
+        if(v.getId()==R.id.createProfilButton){
+            //TODO: switches haben funktion setTextOn setTextOff, automatisieren
+            //TODO: text der edit felder automatisch löschen
+            //where do i get id from, null has to be getId() Function
+            dvb test = new dvb();
+            //String IdArrival = test.getIdFromName(preferredArrival.getText().toString());
+            //String IdDep = test.getIdFromName(preferredDeparture.getText().toString());
+            profile.setPreferredArrival(test.getIdFromName(preferredArrival.getText().toString()),
+                    preferredArrival.getText().toString());
+            profile.setPreferredDeparture(test.getIdFromName(preferredDeparture.getText().toString()),
+                    preferredDeparture.getText().toString());
+//            preferredArrival.setText(IdArrival);
+//            preferredDeparture.setText(IdDep);
+            profile.setProfileName(profilName.getText().toString());
+            //TODO manager.updateProfile(profile, settings);
+//            RoutePlan rout = test.getRoute(IdArrival, IdDep);
+//            preferredDeparture.setText(rout.getDestinationTime());
+        }
+    }
+
+    public void setUi(){
+        wifiSwitch.setChecked(settings.isWifiOn());
+        bluetoothSwitch.setChecked(settings.isBluetoothOn());
+        gpsSwitch.setChecked(settings.isGpsOn());
+        vibrationSwitch.setChecked(settings.isVibrateOn());
+        mobileSwitch.setChecked(settings.isVibrateOn());
     }
 
 }
