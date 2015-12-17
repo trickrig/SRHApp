45522d004aa0a90e74fb9fa9fa989ac037724c58
Robert <srha.20.rschmi@spamgourmet.com>
2015-12-17 13:51:28 +0100

    Restore correct DefaultProfile() behavior

--- app/src/main/java/de/srh/srha/model/ProfileManager.java
+++ app/src/main/java/de/srh/srha/model/ProfileManager.java
@@ -201,7 +201,7 @@ public class ProfileManager {
     private Profile getDefaultProfile() {
         WifiManager wifiManager = (WifiManager) this.activityContext.getSystemService(Context.WIFI_SERVICE);
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
-        Profile p = new Profile(ProfileManager.defaultProfileName, wifiInfo.getSSID(), "33000312", "Zellescher Weg", "33000112", "Zoo");
+        Profile p = new Profile(ProfileManager.defaultProfileName, wifiInfo.getSSID(), "", "", "", "");
         p.settingsManager = new SettingsManager(p, this.activityContext);
         p.settingsManager.setSettings(p.settingsManager.getDefaultSettings(), false);
         return p;
